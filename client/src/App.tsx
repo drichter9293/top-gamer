29b543de67bef71577fdc8a48a32865f91bf49a6
Daniel Richter <drichter9293@gmail.com>
2019-01-16 20:48:13 -0500

    Bootstrap server with express-generator Fetch sample users from server and display on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface User {
+  id: string,
+  username: string
+}
+
+interface AppState {
+  users: Array<User>
+}
+
 class App extends Component {
+  state: AppState = {users: []};
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
